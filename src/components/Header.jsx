e6481c82888d59dd4ba6d9ec9a9761ececd1dd8d
Rahul123223 <rvgarud25@gmail.com>
2025-01-21 16:44:16 +0530

    header add

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/header.css'
+
+const Header = () => {
+  return (
+    <div>
+      <div className='header'>
+        <img src="https://static.naukimg.com/s/0/0/i/naukri-identity/naukri_gnb_logo.svg" alt="Naukri_img" />
+        <p>Jobs</p>
+        <p>Companies</p>
+        <p>Services</p>
+      </div>
+    </div>
+  )
+}
+
+export default Header
